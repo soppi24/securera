6d477108c1696ef6be62ce2b54eb8f43e57eb6bf
soppi24 <sophie.eru@gmail.com>
2025-09-21 03:04:59 -0300

    Lil update: Showing dashboard

--- computation_stuff.py
+++ computation_stuff.py
@@ -1,2 +1,8 @@
-
+import os
+import ipaddress
+import requests
+from dotenv import load_dotenv
+from functools import lru_cache
+import random
+DEMO_MODE = False
 
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import os
 import psutil
 import streamlit as st
 import pandas as pd
 import sqlite3
 from datetime import datetime
 import plotly.express as px
+from dotenv import load_dotenv
+from datetime import datetime
 
+load_dotenv()
+Your_Name = os.getenv("USER_NAME", "DEF_NAME")
+
+
+# THE STREAMLIT INTERFACE
+st.set_page_config(page_title="Network Monitor", page_icon="🛡️", layout="wide")
+st.title("Welcome "+ Your_Name + "!")
+now = datetime.now()
+st.write("Here's a snapshot of your network activity, taken at exactly " + now.strftime("%Y-%m-%d %H:%M:%S"))
